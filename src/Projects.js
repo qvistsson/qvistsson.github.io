f3a979a6a416e9e7fe3ad0a248eb6f70c418cd87
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-09-29 22:41:01 +0200

    re-order projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,13 @@
 const Projects = [
   {
+    title: 'Compare Countries',
+    description:
+      'A graphical interface for comparing information about different countries. Data is fetched from the REST Countries API. Features a cache for already viewed information to reduce API calls, sortable data, and two view modes: spreadsheet and graph view.',
+    tags: ['React', 'API'],
+    viewLink: 'https://qvistdev09.github.io/compare-countries/',
+    gitLink: 'https://github.com/qvistdev09/compare-countries',
+  },
+  {
     title: 'Pomodoro Clock',
     description:
       'A version of the classic productivity tool, with configurable length pomodoros—and breaks!',
@@ -14,22 +22,6 @@ const Projects = [
     tags: ['React'],
     viewLink: 'https://qvistdev09.github.io/javascript-calculator-portfolio/',
     gitLink: 'https://github.com/qvistdev09/javascript-calculator-portfolio',
-  },
-  {
-    title: 'Choropleth Map',
-    description:
-      'Data visualization practice with the D3 library, using data on educational attainment in the US.',
-    tags: ['D3'],
-    viewLink: 'https://qvistdev09.github.io/choropleth-map-portfolio/',
-    gitLink: 'https://github.com/qvistdev09/choropleth-map-portfolio',
-  },
-  {
-    title: 'Heat Map',
-    description:
-      'Another data go at data visualization with D3, this time on average temperatures between 1753-2015.',
-    tags: ['D3'],
-    viewLink: 'https://qvistdev09.github.io/heat-map/',
-    gitLink: 'https://github.com/qvistdev09/heat-map',
   },
   {
     title: 'Markdown Previewer',
@@ -48,12 +40,20 @@ const Projects = [
     gitLink: 'https://github.com/qvistdev09/drum-machine-portfolio',
   },
   {
-    title: 'Compare Countries',
+    title: 'Choropleth Map',
     description:
-      'A graphical interface for comparing information about different countries. Data is fetched from the REST Countries API. Features a cache for already viewed information to reduce API calls, sortable data, and two view modes: spreadsheet and graph view.',
-    tags: ['React', 'API'],
-    viewLink: 'https://qvistdev09.github.io/compare-countries/',
-    gitLink: 'https://github.com/qvistdev09/compare-countries',
+      'Data visualization practice with the D3 library, using data on educational attainment in the US.',
+    tags: ['D3'],
+    viewLink: 'https://qvistdev09.github.io/choropleth-map-portfolio/',
+    gitLink: 'https://github.com/qvistdev09/choropleth-map-portfolio',
+  },
+  {
+    title: 'Tree Map',
+    description:
+      'Another data go at data visualization with D3, this time a tree map representing video game sales.',
+    tags: ['D3'],
+    viewLink: 'https://qvistdev09.github.io/tree-map-portfolio/',
+    gitLink: 'https://github.com/qvistdev09/tree-map-portfolio',
   },
 ];
 
