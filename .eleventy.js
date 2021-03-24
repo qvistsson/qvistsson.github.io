0b247d64c92976bebe14503bdf81be7e39c7c4ff
Oscar Lindqvist <olindqv@gmail.com>
2021-03-24 13:13:57 +0100

    add css modules and scripts folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets');
+  eleventyConfig.addPassthroughCopy('src/scripts');
   return {
     dir: { input: 'src', output: 'dist' },
   };
